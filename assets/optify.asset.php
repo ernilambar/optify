7a0564f99c52a3165404c5b6673fd0fb4e843e9f
Nilambar Sharma <nilambar@outlook.com>
2025-07-30 17:41:05 +0545

    Fix fields

--- assets/optify.asset.php
+++ assets/optify.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '6cfeb6a3cdf33365db61');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '20f710c11cd34930ab49');
