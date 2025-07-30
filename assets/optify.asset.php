ac7eb2532d19aab631f228dcc02f014e70610ebd
Nilambar Sharma <nilambar@outlook.com>
2025-07-30 16:43:02 +0545

    New CSS

--- assets/optify.asset.php
+++ assets/optify.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => 'dbaa1ffc585eb72ec0c7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => 'b9450493e1ebddde6275');
