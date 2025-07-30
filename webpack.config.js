285ea83770d81c3403aeb31db9205db7a6a0188d
Nilambar Sharma <nilambar@outlook.com>
2025-07-30 17:02:27 +0545

    Add spacing in fields

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
 module.exports = {
 	...defaultConfig,
 	entry: {
-		'optify': './resources/index.js',
+		optify: './resources/index.js',
 	},
 	output: {
 		...defaultConfig.output,
 		filename: '[name].js',
-		path: require('path').resolve(__dirname, 'assets'),
+		path: require( 'path' ).resolve( __dirname, 'assets' ),
 	},
 };
