492c218272ced5a03af515714472efdb9f25119a
Nilambar Sharma <nilambar@outlook.com>
2025-07-31 06:10:16 +0545

    Implement versioning

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
+// Prevent cleaning output directory.
+const plugins = defaultConfig.plugins.filter(
+	( plugin ) => plugin.constructor.name !== 'CleanWebpackPlugin'
+);
+
 module.exports = {
 	...defaultConfig,
 	entry: {
@@ -8,6 +13,23 @@ module.exports = {
 	output: {
 		...defaultConfig.output,
 		filename: '[name].js',
+		chunkFilename: '[name].js',
 		path: require( 'path' ).resolve( __dirname, 'assets' ),
+		// Don't clean output directory - let webpack handle file replacement.
+		clean: false,
+	},
+	optimization: {
+		...defaultConfig.optimization,
+		// Disable chunk splitting - bundle everything into one file.
+		splitChunks: false,
+	},
+	// Use filtered plugins
+	plugins: [ ...plugins ],
+	// Enable persistent caching .
+	cache: {
+		type: 'filesystem',
+		buildDependencies: {
+			config: [ __filename ],
+		},
 	},
 };
