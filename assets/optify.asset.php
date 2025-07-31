bd64e44f15c64933eb7547e1d5b40fa971a937cd
Nilambar Sharma <nilambar@outlook.com>
2025-07-31 07:02:40 +0545

    Fix toggle button

--- assets/optify.asset.php
+++ assets/optify.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => 'ca53b37f06111eadfc0e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '1b26dca6996bad87d16a');
