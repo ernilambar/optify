308d79a8339da5b79b3d5ae14cbce05b8ee5d192
Nilambar Sharma <nilambar@outlook.com>
2025-08-09 21:30:48 +0545

    Add icon helper

--- assets/optify.asset.php
+++ assets/optify.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '0a97aeacda9058047140');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '3fa43daafdbf56770857');
