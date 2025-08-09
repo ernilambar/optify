2dae5af9e1af4c76ef3b7c2d02c7e7cc7b2aea4c
Nilambar Sharma <nilambar@outlook.com>
2025-08-09 21:24:27 +0545

    New field renderer

--- assets/optify.asset.php
+++ assets/optify.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => 'ca86782f7a2d20bf474b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '0a97aeacda9058047140');
