b29b55c2d4fd89a77925c4de55290ec4fed7e945
Nilambar Sharma <nilambar@outlook.com>
2025-08-11 22:55:21 +0545

    Update submit status

--- assets/optify.asset.php
+++ assets/optify.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '73e3b24442d9d46ae1a9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '217f035ff95890d1d365');
