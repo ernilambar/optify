100a2dfe789b5c369cb2477dde5d448d249d64fa
Nilambar Sharma <nilambar@outlook.com>
2025-08-18 18:28:49 +0545

    Conditional

--- assets/optify.asset.php
+++ assets/optify.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '38965308bf34d83ed090');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-i18n'), 'version' => '1b460207ca216b9d7a0d');
